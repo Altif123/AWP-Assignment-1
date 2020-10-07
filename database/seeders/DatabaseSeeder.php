9c62c991f7a38eb5dfe93990351c39b22a2e404b
Altif123 <altifali.aa@gmail.com>
2020-10-07 22:40:26 +0100

    Added menu table and seeder and css change to home page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Database\Seeders;
+//namespace Database\Seeders;
 
+use Database\Seeders\MenuSeeder;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this ->call(MenuSeeder::class);
     }
 }
