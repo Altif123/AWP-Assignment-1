ea427de9188d655722f26dd28c39d886fbd008c6
Altif123 <altifali.aa@gmail.com>
2020-10-07 22:37:58 +0100

    Added menu table and seeder and css change to home page

---
+++ database/seeders/MenuSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MenuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('menu')->insert([
+            'dish_name' => 'Beans on toast',
+            'description' => 'Crispy toast with a dollop of beans and a sprinkle of cheese',
+            'amount' => 2.99,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+
+        ]);
+    }
+}
