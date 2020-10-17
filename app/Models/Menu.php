5b50bc984f257f56621174ff0bd8004093b32024
Altif123 <altifali.aa@gmail.com>
2020-10-17 23:00:23 +0100

    added route paths

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -9,4 +9,29 @@ class Menu extends Model
 {
     use HasFactory;
     protected $table = 'menu';
+
+    protected $fillable = ['dish_name','description','allergy','price'];
+
+    public function indexPath()
+    {
+        return "/menu/";
+    }
+    public function showPath()
+    {
+        return "menu/". $this -> id;
+    }
+
+    public function editMenuPath()
+    {
+        return $this -> id . '/edit';
+    }
+    public function deleteMenuPath()
+    {
+        return $this -> id . '/delete';
+    }
+    public function createMenuPath()
+    {
+        return 'menu/create';
+        //use with post and get
+    }
 }
