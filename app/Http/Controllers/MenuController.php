c6e0c3e0a74f2f0731087d7aaf071ed1f03c5a41
Altif123 <altifali.aa@gmail.com>
2020-10-18 20:37:30 +0100

    Cleaned up menu controller

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -8,121 +8,71 @@ use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    /**
-     * Display a listing of the all the records in the DB.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        $showall = Menu::all()->sortBy("dish_name");
-
-        return view('menu/index',['menu' => $showall]);
+        return view('menu/index', [
+            'menu'=> Menu::latest()->get()]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('menu.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+
+    public function store()
     {
-        $request->validate([
+        Menu::create($this->validateMenuItem());
+
+        return redirect('menu/');
+    }
+
+    public function show(Menu $menuItem)
+    {
+
+        return view('menu.show', compact('menuItem'));
+    }
+
+
+    public function edit(Menu $menuItem)
+    {
+
+        return view('menu.update',  compact('menuItem'));
+    }
+
+
+    public function update(Request $request,Menu $menuItem)
+    {
+
+
+        $menuItem->update( $this->validateMenuItem());
+
+        return redirect('menu/');
+
+    }
+
+
+    public function destroy(Menu $menuItem)
+    {
+        $menuItem->delete();
+        return redirect('menu/');
+    }
+
+    /**
+     * @return array of validated fields
+     */
+
+    protected function validateMenuItem()
+    {
+
+        return request()->validate([
             'dish_name' => ['required', 'max:200'],
             'description' => ['required'],
-            'price' => ['required', 'max:5','min:4'],
+            'allergy' => ['required'],
+            'price' => ['required', 'max:5', 'min:4'],
         ]);
 
-        $menu = new Menu();
-        $menu-> dish_name =request('dish_name');
-        $menu-> description =request('description');
-        $menu-> allergy =request('allergy');
-        $menu-> price =request('price');
-        $menu ->save();
-
-       return redirect('menu/');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Menu  $menu
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $menuItem = Menu::find($id);
-
-        return view('menu.show', ['menuitem'=> $menuItem]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Menu  $menu
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //find article with id
-        $menuItem = Menu::find($id);
-
-        return view('menu.update', ['item'=> $menuItem]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Menu  $menu
-     * @return \Illuminate\Http\Response
-     */
-    public function update($id)
-    {
-        //find article with id
-        $menu = Menu::find($id);
-
-//        $request->validate([
-//            'dish_name' => ['required', 'max:200'],
-//            'description' => ['required'],
-//            'amount' => ['required', 'max:5','min:5'],
-//        ]);
-
-
-        $menu-> dish_name =request('dish_name');
-        $menu-> description =request('description');
-        $menu-> allergy =request('allergy');
-        $menu-> price =request('price');
-        $menu ->save();
-
-        return redirect('menu/');
-
-    }
-
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Menu $menu
-     * @return \Illuminate\Http\Response
-     * @throws \Exception
-     */
-    public function destroy($id)
-    {
-        $menu = Menu::find($id);
-
-        $menu -> delete();
-        return redirect('menu/');
     }
 }
