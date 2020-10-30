15de5cc768746f42432f630978ce7e647388fddc
Altif123 <altifali.aa@gmail.com>
2020-10-30 12:44:00 +0000

    Used named routes where possible and fixed line spacing

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -12,29 +12,20 @@ class FavoriteController extends Controller
     public function index()
     {
         $favorites = User::find(auth()->user()->id)->getfavorites;
-
-        return view('favorite.index', ['favorites' => $favorites]);
-
+        return view('favorite.index', compact('favorites'));
     }
 
     public function store($menuItem)
     {
-
         $favorite = new Favorite();
-
         $favorite->favorites()->attach(auth()->user()->id, ['menu_id' => $menuItem]);
-
-
-        return redirect('favorites/');
+        return redirect()->route('favorites.index');
     }
-
 
     public function destroy(Favorite $favorite, $favoriteItem)
     {
-
-
         $favorite->where(['menu_id' => $favoriteItem, 'user_id' => auth()->user()->id])->delete();
-
-        return redirect('favorites/');
+        return redirect()->route('favorites.index');
     }
 }
+
--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,72 +4,57 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Menu;
-use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class MenuController extends Controller
 {
-
     public function index()
     {
         return view('menu/index', [
             'menu' => Menu::latest()->get()]);
     }
 
-
     public function create()
     {
         return view('menu.create');
     }
 
-
     public function store()
     {
         Menu::create($this->validateMenuItem());
-
-        return redirect('menu/');
+        return redirect()->route('menu.index');
     }
 
     public function show(Menu $menuItem)
     {
-
         return view('menu.show', compact('menuItem'));
     }
 
-
     public function edit(Menu $menuItem)
     {
-
         return view('menu.update', compact('menuItem'));
     }
 
-
-    public function update(Request $request, Menu $menuItem)
+    public function update(Menu $menuItem)
     {
-
-
         $menuItem->update($this->validateMenuItem());
-
-        return redirect('menu/');
-
+        return redirect()->route('menu.index');
     }
-
 
     public function destroy(Menu $menuItem)
     {
         $menuItem->delete();
-        return redirect('menu/');
+        return redirect()->route('menu.index');
     }
-
 
     protected function validateMenuItem()
     {
-
         return request()->validate([
             'dish_name' => ['required', 'max:200'],
             'description' => ['required'],
             'allergy' => ['required'],
             'price' => ['required', 'max:5', 'min:4'],
         ]);
-
     }
 }
+
