19613e3b8189751f463e7f7d2731c44e4ac95587
Altif123 <altifali.aa@gmail.com>
2020-12-17 16:13:47 +0000

    Fixed back button search bug and added password reset using mailtrap

--- resources/views/components/backBtn.blade.php
+++ resources/views/components/backBtn.blade.php
@@ -1,4 +1,4 @@
 <div class="px-6 pt-4 pb-2 " role="button">
-    <a href="{{url()->previous()}}"> <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
+    <a href="{{route('menu.index')}}"> <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm
     font-semibold text-gray-700 mr-2 mb-2 fas fa-arrow-left">Back</span></a>
 </div>
